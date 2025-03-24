70fbf7779e69378dbc5a5d0b97f8d578816c323f
PankajKumar17 <pankajfriend17910@gmail.com>
2025-03-25 01:39:11 +0530

    fixed some bugs

--- server/utils/fetchImage.js
+++ server/utils/fetchImage.js
@@ -8,7 +8,7 @@ export const fetchImage = async (query) => {
             return data.items[0].link;
         } else {
             console.log("No image found for:", query);
-            return "https://i.gifer.com/ZKZg.gif";
+            return "https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg";
         }
     } catch (error) {
         console.error("Error fetching image for:", query, error);
